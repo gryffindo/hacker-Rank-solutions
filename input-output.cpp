ca820ef7293e220dd565380e5e8299b45814f6f9
Sakib Al Hasan <76560286+saifsaki@users.noreply.github.com>
2021-07-14 01:41:03 +0600

    solved input output

---
+++ input-output.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i = 0;
+    int sum = 0;
+    
+    while (cin >> i) {
+        sum += i;
+    }
+    
+    cout << sum << endl;
+    
+    return 0;
+}
+
+// Problem Link: https://www.hackerrank.com/challenges/cpp-input-and-output/problem
